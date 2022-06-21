99de8f24410630f6d9b03716b2b457ed042ae4b9
Adam Wojnicki <a.wojnicki@commtree.ltd>
2022-06-21 14:53:10 +0200

    minor change

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("asdf");
 });
+app.get("/aa", (req, res) => {
+  res.send("aa");
+});
 
 app.listen(4000, () => {
   console.log("App working on post 4000");
